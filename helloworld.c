321f6c78f8b077b226743c44abb0e998c6cd6ffa
krazykoder <noddy4nods@gmail.com>
2019-01-05 09:59:58 -0800

    Changed 2 files; added func addme (a,b)

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,12 @@
 #include <stdio.h> 
 
+int addme (int a, int b) {
+	return (a+b);
+}
+
+
 int main () {
-	printf ("Hello World from Unix \n"); 
-	return 0; 
+	printf ("Hello World from Unix. I am Krazykoder \n");
+	printf("I added: 5 + 7 = %d\n", addme(5,7));
+	return 0;
 }
--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,7 @@
-print ("Hello World from Unix") 
+def addme ( a, b):
+	return a+b 
+
+print ("Hello World from Unix. I am Krazykoder")
+print ( "I added: 5 + 7 = ", addme(5,7))
+
 
